ad1d5f2802413f781116a1e61e64a70d234aee27
EuronG <juandigiraldoj@gmail.com>
2023-05-26 12:10:10 -0500

    css final

---
+++ coursera project 3/gallery.js
@@ -0,0 +1,11 @@
+function upDate(previewPic){
+    let x = document.getElementById("image");
+    x.style.backgroundImage = "url(" + previewPic.src + ")";
+    x.innerHTML = previewPic.alt;
+    }
+   
+function unDo(){
+    let x = document.getElementById("image");
+    x.style.backgroundImage = "url('')";
+    x.innerHTML = "Hover over an image below to display here."
+    }
